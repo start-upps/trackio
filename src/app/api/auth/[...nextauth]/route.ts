d1a1752732adcfec4bbef927e94e3b97d8d74b96
Ismat <ismetsemedov@gmail.com>
2024-11-28 18:35:52 +0400

    check

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,9 @@
 // src/app/api/auth/[...nextauth]/route.ts
-import { authOptions } from "@/lib/auth"
 import NextAuth from "next-auth"
+import { authOptions } from "@/lib/auth"
 
 const handler = NextAuth(authOptions)
 
-export const GET = handler
-export const POST = handler
+export const GET = handler.auth
+export const POST = handler.auth
+export const runtime = "nodejs"
