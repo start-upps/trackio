720c28e3bcf820ac42dfefb83b4ade7a1b1359b7
Ismat <ismetsemedov@gmail.com>
2024-11-28 18:17:33 +0400

    sign in errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 // src/app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth"
+import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+
+export const { GET, POST } = handler
+export const runtime = "nodejs"
